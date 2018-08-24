43e3ccc9e1ffcfe11352e449b6e6bf2defce3dc5
Michał Jagieło <42613646+michjag@users.noreply.github.com>
2018-08-24 15:38:25 +0200

    Update struct.cpp

--- struct.cpp
+++ struct.cpp
@@ -13,10 +13,16 @@ int main()
 {
     Batonik snack[4]=
     {
-         {"Mocha Munch", 2.3, 350}
+        {"Mocha Munch", 2.3, 350},
+        {"Sniki", 5.8, 700},
+        {"Wenus", 9.9, 1000},
+        {"Mokk Munci", 1.2, 150}
     };
 
-cout<<snack[0].marka<<" "<<snack[0].waga<<" "<<snack[0].kcal;
+cout<<snack[0].marka<<" "<<snack[0].waga<<" "<<snack[0].kcal<<endl;
+cout<<snack[1].marka<<" "<<snack[1].waga<<" "<<snack[1].kcal<<endl;
+cout<<snack[2].marka<<" "<<snack[2].waga<<" "<<snack[2].kcal<<endl;
+cout<<snack[3].marka<<" "<<snack[3].waga<<" "<<snack[3].kcal<<endl;
 
 return 0;
 }
