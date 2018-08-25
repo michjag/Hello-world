bab8d61e0172275edcdaa60318556ebdc956eef9
Michał Jagieło <42613646+michjag@users.noreply.github.com>
2018-08-25 10:48:41 +0200

    Create struct-pizza.cpp

---
+++ struct-pizza.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct Pizza
+{
+    string marka;
+    int srednica;
+    int waga;
+};
+
+int main()
+{
+    Pizza jeden;
+    {
+cout<<"Podaj marke: "<<endl;
+getline (cin, jeden.marka);
+cout<<"Podaj srednice: "<<endl;
+cin>>jeden.srednica;
+cout<<"Podaj wage: "<<endl;
+cin>>jeden.waga;
+    };
+
+cout<<"Marka "<<jeden.marka<<endl;
+cout<<"Srednica: "<<jeden.srednica<<endl;
+cout<<"Waga: "<<jeden.waga<<endl;
+
+   return 0;
+
+}
